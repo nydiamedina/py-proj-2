6ebc3b59c154b1c15989538c68dbc597e289200c
Nydia Medina <nvmedina29@gmail.com>
2022-11-30 07:24:46 -0600

    Add 'Add to Cart functionality'

--- starter/cupcakes.py
+++ starter/cupcakes.py
@@ -113,6 +113,28 @@ def get_cupcakes(file):
         return reader
 
 
+def find_cupcake(file, name):
+    for cupcake in get_cupcakes(file):
+        if cupcake["name"] == name:
+            return cupcake
+    return None
+
+
+def add_cupcake_dictionary(file, cupcake):
+    with open(file, "a", newline="\n") as csvfile:
+        fieldnames = [
+            "size",
+            "name",
+            "price",
+            "flavor",
+            "frosting",
+            "sprinkles",
+            "filling",
+        ]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(cupcake)
+
+
 def add_cupcake(file, cupcake):
     with open(file, "a", newline="\n") as csvfile:
         fieldnames = [
--- starter/server.py
+++ starter/server.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template
-from cupcakes import get_cupcakes
+from flask import Flask, render_template, url_for, redirect
+from cupcakes import get_cupcakes, find_cupcake, add_cupcake_dictionary
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", cupcakes = get_cupcakes("cupcakes.csv"))
+    return render_template("index.html", cupcakes=get_cupcakes("cupcakes.csv"))
 
 
 @app.route("/cupcakes")
 def all_cupcakes():
-    return render_template("cupcakes.html", cupcakes = get_cupcakes("cupcakes.csv"))
+    return render_template("cupcakes.html", cupcakes=get_cupcakes("cupcakes.csv"))
 
 
 @app.route("/cupcake-individual")
@@ -22,6 +22,17 @@ def individual_cupcake():
 @app.route("/order")
 def order():
     return render_template("order.html")
+
+
+@app.route("/add-cupcake/<name>")
+def add_cupcake(name):
+    cupcake = find_cupcake("cupcakes.csv", name)
+
+    if cupcake:
+        add_cupcake_dictionary("orders.csv", cupcake)
+        return redirect(url_for("home"))
+    else:
+        return "Sorry cupcake not found."
 
 
 if __name__ == "__main__":
