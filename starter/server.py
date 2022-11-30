7fbbcf21c480b11cf367172bcaeecd5f0f05b389
Nydia Medina <nvmedina29@gmail.com>
2022-11-30 05:10:00 -0600

    Add order and cupcake HTML templates

--- starter/server.py
+++ starter/server.py
@@ -8,6 +8,21 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/cupcakes")
+def all_cupcakes():
+    return render_template("cupcakes.html")
+
+
+@app.route("/cupcake-individual")
+def individual_cupcake():
+    return render_template("individual-cupcake.html")
+
+
+@app.route("/order")
+def order():
+    return render_template("order.html")
+
+
 if __name__ == "__main__":
     app.env = "development"
     app.run(debug=True, port=8000, host="localhost")
